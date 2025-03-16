f88910d7449eb858df045131f1a61965f604781f
Yagho Petini <yaghojp@gmail.com>
2025-03-15 21:32:17 -0300

    frontend feito

---
+++ frontend/src/app/dashboard/components/modal/index.tsx
@@ -0,0 +1,56 @@
+"use client"
+
+import styles from './styles.module.scss'
+import { X } from 'lucide-react'
+import { use } from 'react'
+import { OrderContext } from '@/providers/order'
+
+export function Modalorder(){
+  const { onRequestClose, order, finishOrder } = use(OrderContext);
+
+  async function handleFinishOrder() {
+    console.log("Entrou na função")
+    await finishOrder(order[0].order.id)
+  }
+
+  return(
+    <dialog className={styles.dialogContainer}>
+
+     <section className={styles.dialogContent}>
+        <button className={styles.dialogBack} onClick={onRequestClose}>
+          <X size={40} color="#FF3f4b" />
+        </button>
+
+        <article className={styles.container}>
+          <h2>Detalhes do pedido</h2>
+          
+          <span className={styles.table}>
+            Mesa <b>{order[0].order.table}</b>
+          </span>
+
+          {order[0].order?.name && (
+          <span className={styles.name}>
+            <b>{order[0].order.name}</b>
+          </span>
+          )}
+
+          {order.map( item => (
+            <section className={styles.item} key={item.id}>
+            <span>{item.amount} - <b>{item.product.name}</b></span>
+            <span className={styles.description}>
+              {item.product.description}
+            </span>
+          </section>             
+          ))}      
+
+          <button className={styles.buttonOrder} onClick={handleFinishOrder}>
+            Concluir pedido
+          </button>
+
+        </article>
+
+     </section>
+
+    </dialog>
+  )
+}
---
+++ frontend/src/app/dashboard/components/orders/index.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import { use } from 'react'
+import styles from './styles.module.scss'
+import { RefreshCw } from 'lucide-react'
+import { OrderProps } from '@/lib/order.type'
+import { Modalorder } from '@/app/dashboard/components/modal'
+import { OrderContext } from '@/providers/order'
+
+interface Props{
+  orders: OrderProps[]
+}
+
+export function Orders({ orders }: Props){
+  const { isOpen, onRequestOpen } = use(OrderContext)
+  //console.log(orders)
+  async function handleDetailOrder(order_id: string){
+    
+    await onRequestOpen(order_id)
+  }
+
+  return(
+  <>
+    <main className={styles.container}>
+
+      <section className={styles.containerHeader}>
+        <h1>Últimos pedidos</h1>
+        <button>
+          <RefreshCw size={24} color="#3fffa3" />
+        </button>
+      </section>
+
+      <section className={styles.listOrders}>
+        {orders.map( order => ( 
+          <button
+            key={order.id}
+            className={styles.orderItem}
+            onClick={ () => handleDetailOrder(order.id)}
+          >
+            <div className={styles.tag}></div>
+            <span>Mesa {order.table}</span>
+          </button>  
+        ))}
+      </section>
+     
+    </main>
+
+    { isOpen && <Modalorder/> }
+  </>
+  )
+}
